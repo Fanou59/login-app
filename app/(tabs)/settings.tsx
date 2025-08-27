a13bbaf188b26ac99be639fe73544e2e154833d1
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-08-27 15:28:16 +0200

    feat(ui): ajouter des composants Divider et Pressable, mettre à jour l'écran des paramètres et supprimer l'écran de profil

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,1 +1,35 @@
-export default function SettingsScreen() {}
+import { Divider } from "@/components/ui/divider";
+import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
+import { Icon } from "@/components/ui/icon";
+import { Pressable } from "@/components/ui/pressable";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { ChevronRight, UserRound } from "lucide-react-native";
+
+export default function SettingsScreen() {
+  return (
+    <VStack className="flex-1 pt-4 px-8">
+      <Heading size="3xl" className="mb-6">
+        Settings
+      </Heading>
+      <HStack className="items-center gap-1">
+        <Icon as={UserRound}></Icon>
+        <Heading size="xl">Compte</Heading>
+      </HStack>
+      <Divider className="mt-3" />
+      <Pressable className="py-3">
+        <HStack className="items-center justify-between">
+          <Text>Modifier le profil</Text>
+          <Icon as={ChevronRight} size="sm" />
+        </HStack>
+      </Pressable>
+      <Pressable className="py-3">
+        <HStack className="items-center justify-between">
+          <Text>Changer le mot de passe</Text>
+          <Icon as={ChevronRight} size="sm" />
+        </HStack>
+      </Pressable>
+    </VStack>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,19 @@ import { Stack } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { AuthProvider } from "@/contexts/AuthContext";
 import "@/global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <GluestackUIProvider mode="light">
       <AuthProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(tabs)" />
-        </Stack>
+        <SafeAreaView style={{ flex: 1, paddingTop: 20 }}>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(tabs)" />
+          </Stack>
+        </SafeAreaView>
       </AuthProvider>
     </GluestackUIProvider>
   );
