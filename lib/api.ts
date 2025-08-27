6ffb31a87baa0dd1cd79e83c60172390db801827
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-08-27 13:57:38 +0200

    fix(api): corriger l'URL de préfixe de l'API pour correspondre à la configuration correcte

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import ky from "ky";
 
 // Configuration de base de votre API
 export const api = ky.create({
-  prefixUrl: "http://127.0.0.1:8001/api", // Remplacez par votre URL d'API
+  prefixUrl: "http://127.0.0.1:8000/api", // Remplacez par votre URL d'API
   timeout: 10000,
   headers: {
     "Content-Type": "application/ld+json",
