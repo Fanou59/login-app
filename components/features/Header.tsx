cacd6f7e0b3ff15d3b8d006b11241d5d19feb7c5
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2026-01-06 19:15:52 +0100

    UI du header

--- components/features/Header.tsx
+++ components/features/Header.tsx
@@ -5,15 +5,17 @@ import { Image } from "react-native";
 export function Header() {
   return (
     <VStack className="items-center" space="xs">
-      <Image
-        source={require("@/assets/images/trail-ready-logo.png")}
-        style={{
-          width: 60,
-          height: 60,
-          resizeMode: "contain",
-        }}
-      />
-      <Text className="text-3xl">Trail Ready</Text>
+      <VStack className="flex flex-row items-center" space="sm">
+        <Image
+          source={require("@/assets/images/trail-ready-logo.png")}
+          style={{
+            width: 50,
+            height: 50,
+            resizeMode: "contain",
+          }}
+        />
+        <Text className="text-3xl">Trail Ready</Text>
+      </VStack>
       <Text className="text-base">Votre expérience commence ici</Text>
     </VStack>
   );
